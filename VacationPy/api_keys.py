d92e36191f5d9547f102da1c4e37d39a5367dd35
msifontesg <119377808+msifontesg@users.noreply.github.com>
2023-05-22 19:11:07 -0500

    Add files via upload

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "1a4bd1d6fab24e4964830f7c2d5100b2"
+
+# Geoapify API Key
+geoapify_key = "12974f503c7c4c439c9fbe66078c2dd9"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "1a4bd1d6fab24e4964830f7c2d5100b2"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
